9f864b58b45aceee5c107feb792b889243c5db66
Chakirtkbr <chakritsixtyfour@gmail.com>
2024-10-29 16:06:49 +0700

    setup docker-compose for DB and user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+import { Post } from './post/entities/post.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'adminBoardTest',
+      password: 'BoardTest1234',
+      database: 'boardTest',
+      entities: [User, Post, Comment],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
